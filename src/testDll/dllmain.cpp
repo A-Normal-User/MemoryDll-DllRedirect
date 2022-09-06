6a4f077f94cfbfcd84237a99e2ab1938a0948503
A-Normal-User <1185384801@qq.com>
2022-09-06 16:03:38 +0800

    Add files via upload

--- src/testDll/dllmain.cpp
+++ src/testDll/dllmain.cpp
@@ -1,1 +1,21 @@
+﻿// dllmain.cpp : 定义 DLL 应用程序的入口点。
+#include "pch.h"
+#include "windows.h"
+
+BOOL APIENTRY DllMain( HMODULE hModule,
+                       DWORD  ul_reason_for_call,
+                       LPVOID lpReserved
+                     )
+{
+    switch (ul_reason_for_call)
+    {
+    case DLL_PROCESS_ATTACH:
+        MessageBoxW(0, L"如果你看到我，说明Dll修改成功了！", L"温馨提示", 64);
+    case DLL_THREAD_ATTACH:
+    case DLL_THREAD_DETACH:
+    case DLL_PROCESS_DETACH:
+        break;
+    }
+    return TRUE;
+}
 
